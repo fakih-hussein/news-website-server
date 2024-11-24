d57755107a8c3c80ecf3d013cc3e5d32e2994e69
fh95-tech <fakih.hussein995@gmail.com>
2024-11-24 12:04:58 +0200

    created post news controller

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\News;
 
-class NewsController extends Controller
-{
-    //
+class NewsController extends Controller{
+    function post_news(Request $request){
+        $news = News::create([
+            "title"=>$request->title,
+            "content"=>$request->content,
+            "is_restricted"=>$request->is_restricted,
+
+        ]);
+    }
 }
