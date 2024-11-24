a53551a36eaba272d396d5082618b12181175626
fh95-tech <fakih.hussein995@gmail.com>
2024-11-24 12:06:18 +0200

    created News model

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
-{
-    //
+class News extends Model{
+    protected $fillable=["title","content","is_restricted"];
 }
